2e35d3d204c87e4965c215263c71884aba775e98
lemoncat0817 <king123456789abc@gmail.com>
2024-06-13 19:34:05 +0800

    bug fix

--- README.md
+++ README.md
@@ -68,3 +68,4 @@ pnpm dev
 pnpm build
 ```
 
+
--- assets/layoutSetting-Ba4WGt3Z.js
+++
@@ -1,1 +0,0 @@
-import{z as o,m as t}from"./index-Dl-Mm33K.js";const a=o("layoutSetting",()=>{const e=t(!1),r=t("rgba(255, 69, 0, 0.68)");return{dark:e,color:r}},{persist:!0});export{a as u};
